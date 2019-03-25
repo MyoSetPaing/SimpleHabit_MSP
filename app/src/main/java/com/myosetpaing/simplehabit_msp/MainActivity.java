10fb1c0c9f8bce22a79d406824c0f50c9e8e83a4
Myo Set Paing <myosetpaing@aceplussolutions.com>
2019-03-25 16:14:50 +0630

    create project

---
+++ app/src/main/java/com/myosetpaing/simplehabit_msp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.myosetpaing.simplehabit_msp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
